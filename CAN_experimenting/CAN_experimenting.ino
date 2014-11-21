166218654c2fbad4d3efe918737f0dbd0b69ad48
Asaurus1 <fauxjunk-1@yahoo.com>
2014-11-20 21:01:32 -0600

    Fixed Filters
    Filters now work, but interface needs to be improved.

--- CAN_experimenting/CAN_experimenting.ino
+++ CAN_experimenting/CAN_experimenting.ino
@@ -4,16 +4,17 @@
 #include "CAN_IO.h"
 
 CAN_IO can(4,5);
-FilterInfo filters {0xFF8,0, DC_DRIVE_ID,0,0,0,0,0}; //Set up masks and filters. All of them 0 for now.
+FilterInfo filters {0x0FF0,0x0FFF, 0x501,0,0,0,0,0}; //Set up masks and filters. All of them 0 for now.
 
 void setup()
 {
-  can.setup(filters);
   Serial.begin(9600);
+  can.setup(filters);
   
+  // Read Filter bits for RB0 to make sure that they are correct.
 }
 
-/* For TX*/
+/* For TX
 void loop()
 {
   DC_Drive packet(40,5); // Create drive command, vel = 40, cur = 5;
@@ -22,23 +23,26 @@ void loop()
   delay(500);
   can.send_CAN(packet2);
   delay(500);
-}
+}*/
 
-/* For RX
+/* For RX*/
 void loop()
 {
   byte errors = 0x0000;
   if (digitalRead(5) == LOW)
   {
+     Serial.println(can.controller.RXStatus(),BIN);
      can.receive_CAN(errors); //Reads CAN data into buffer
+     
   }
   
   if (can.messageavailable) {
     DC_Drive packet(can.buffer[can.buffer_index]); //Get the drive packet
     Serial.println(packet.velocity);
     Serial.println(packet.current);
-    Serial.println(packet.id);
+    Serial.println(packet.id,HEX);
     delay(250);
+    can.messageavailable = false;
   }
   else
   {
@@ -46,5 +50,5 @@ void loop()
     Serial.println(errors,BIN);
     delay(250);
   }
-}*/
+}
   
