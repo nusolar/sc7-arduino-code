03dd7e8947646929f48de6b1eb214b81de189a29
Asaurus1 <fauxjunk-1@yahoo.com>
2015-02-07 13:27:40 -0600

    Communicates switch state to Serial
    Switches are still a little bit mechanically faulty. Working on dealing
    with that.

--- SWControls/SWControls.ino
+++ SWControls/SWControls.ino
@@ -1,8 +1,7 @@
+#include "sc7-can-libinclude.h"
 #include <Metro.h>
 
 #include <SPI.h>
-
-#include <sc7-can-libinclude.h>
  
  #define BIT(n)                  ( 1<<(n) ) 
  #define BIT_SET(y, mask)        ( y |=  (mask) ) 
@@ -22,14 +21,14 @@ byte young;
 byte old;
 
 //set up pins that connect to switch terminals
-   const int fgp = ;
-   const int rgp = ;
-   const int hp = ;
-   const int hzp = ;
-   const int ccp = ;
-   const int hornp = ;
-   const int ltp = ;
-   const int rtp = ;
+   const int fgp =   12;
+   const int rgp =   11;
+   const int hp =    10;
+   const int hzp =    9;
+   const int ccp =    2;
+   const int hornp =  3;
+   const int ltp =    4;
+   const int rtp =    5;
 
 //set up metro timer
   //1st: switch state reading timer
@@ -53,20 +52,20 @@ const uint16_t RXF2      = MASK_NONE;
 const uint16_t RXF3      = MASK_NONE;
 const uint16_t RXF4      = MASK_NONE;
 const uint16_t RXF5      = MASK_NONE;
-byte 		   CAN_errors;
-
+      uint16_t CAN_errors;
+      
 CAN_IO canControl(CAN_CS,CAN_INT,CAN_BAUD_RATE,CAN_FREQ);
 
  void setup() {
    // Pin Modes
-  	pinMode(fgp, INPUT);
-	pinMode(rgp, INPUT);
-	pinMode(hp, INPUT);
-	pinMode(hzp, INPUT);
-	pinMode(ccp, INPUT);
-	pinMode(hornp, INPUT);
-	pinMode(ltp, INPUT);
-	pinMode(rtp, INPUT);
+  	pinMode(fgp, INPUT_PULLUP);
+	pinMode(rgp, INPUT_PULLUP);
+	pinMode(hp, INPUT_PULLUP);
+	pinMode(hzp, INPUT_PULLUP);
+	pinMode(ccp, INPUT_PULLUP);
+	pinMode(hornp, INPUT_PULLUP);
+	pinMode(ltp, INPUT_PULLUP);
+	pinMode(rtp, INPUT_PULLUP);
 
 /*SPST - left turn, right turn, horn, cruise control
 SPDT - forward/neutral/reverse, headlight/no light/hazard*/
@@ -78,13 +77,13 @@ SPDT - forward/neutral/reverse, headlight/no light/hazard*/
     CANFilterOpt filter;
     filter.setRB0(MASK_NONE,DC_DRIVE_ID,0);
     filter.setRB1(MASK_NONE,DC_SWITCHPOS_ID,0,0,0);
-    can.Setup(filter, &CAN_errors);
+    canControl.Setup(filter, &CAN_errors);
 
 }
 
 inline void setyoungbit(byte pin, byte& out, byte mask){
   if (digitalRead(pin)==1){
-    BIT_SET(out),mask);
+    BIT_SET(out,mask);
   }
   else{
     BIT_CLEAR(out,mask);
@@ -98,7 +97,7 @@ void loop() {
     old = young; // Store old switch values.
     setyoungbit(fgp,  young,FWD_GEAR);
     setyoungbit(rgp,  young,REV_GEAR);
-    setyoungbit(hp,	  young,HEADLIGHT);
+    setyoungbit(hp,   young,HEADLIGHT);
     setyoungbit(hzp,  young,HAZARDLIGHT);
     setyoungbit(ccp,  young,CRUISE_CONTROL);
     setyoungbit(hornp,young,HORN);
@@ -133,7 +132,8 @@ void loop() {
   /* Write to serial for testing */
   if (young != old || CAN_TX.check())
   {
-	Serial.write(young,BIN);
+	Serial.println(young,BIN);
+        old = young;
   }
 
 }                       
