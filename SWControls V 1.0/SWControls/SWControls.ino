7760adadd38eb1ff900095d762d691c831589f80
cpraf104 <ethanpark2018@u.northwestern.edu>
2015-03-16 19:34:40 -0500

    SWControls fancy dancy version 1.0 folder

--- SWControls V 1.0/SWControls/SWControls.ino
+++ SWControls V 1.0/SWControls/SWControls.ino
@@ -8,14 +8,18 @@
 //#define LOOPBACK
 //#define DEBUG
 
+/*Defining the bitwise functions (bitwise operators)
+We're using bits to store data because there are only 8 bytes available for use in a CAN packet.
+We can store all of the necessary data in a single byte and save space. This frees up space to
+send other data that we might need in the future between the steering wheel and the driver controls*/
 #define BIT(n)                   ( 1<<(n) ) 
-#define BIT_SET(y, mask)         ( y |=  (mask) ) 
-#define BIT_CLEAR(y, mask)       ( y &= ~(mask) ) 
-#define BIT_FLIP(y, mask)        ( y ^=  (mask) )
-#define BIT_DIFFERENT(y, x, mask)( y & mask == x & mask )
-#define BIT_CHECK(y, bit, mask)  ( BIT_DIFFERENT(y, (bit)*mask, mask) )
+#define BIT_SET(y, mask)         ( y |=  (mask) )                       //Set the value of BIT = 1
+#define BIT_CLEAR(y, mask)       ( y &= ~(mask) )                       //Set the value of BIT = 0
+#define BIT_FLIP(y, mask)        ( y ^=  (mask) )                       //Flip the value of BIT
+#define BIT_DIFFERENT(y, x, mask)( y & mask == x & mask )               //See if two bits are different values
+#define BIT_CHECK(y, bit, mask)  ( BIT_DIFFERENT(y, (bit)*mask, mask) ) //check function utilizing the previous defined different function
 
-/* Bit masks */
+//Defining the bit masks
 #define FWD_GEAR BIT(0)
 #define REV_GEAR BIT(1)
 #define HEADLIGHT BIT(2)
@@ -24,38 +28,42 @@
 #define HORN BIT(5)
 #define LEFT_TURN BIT(6)
 #define RIGHT_TURN BIT(7)
-char young = 0xFF;
-char old;
 
-//Steering Wheel LCD Info
-const int SOC = 8;
-const int V = 6;
-const int GEAR = 13;
-const int CC = 11;
-const int LIGHT = 12;
-const int RIGHT = 15;
-const int LEFT = 1;
+/*These are the bytes containing the above-mentioned bits. The two bytes will be used
+to compare the switch states and determine whether there has been a change or not.*/
+char young = 0xFF; //young is continuously assigned to the new switch states
+char old;          //old is the previous switch states
+
+
+//Steering Wheel LCD Info - the position on the LCD
+const int SOC = 8;    //state of charge (from CAN)
+const int V = 6;      //velocity (from CAN)
+const int GEAR = 13;  //forward/reverse/neutral
+const int CC = 11;    //cruise control
+const int LIGHT = 12; //headlights/hazardlights/no lights
+const int RIGHT = 15; //right turn signals
+const int LEFT = 1;   //left turn signals
 
 //set up pins that connect to switch terminals
-const int fgp =   9;
-const int rgp =   8;
-const int hp =    7;
-const int hzp =   6;
-const int ccp =   3;
-const int hornp = 10;
-const int ltp =   4;
-const int rtp =   5;
+const int fgp =   9;  //forward gear
+const int rgp =   8;  //reverse gear
+const int hp =    7;  //headlights
+const int hzp =   6;  //hazardlights
+const int ccp =   3;  //cruise control
+const int hornp = 10; //horn
+const int ltp =   4;  //left turn
+const int rtp =   5;  //right turn
 
 //set up metro timer
-//1st: switch state reading timer
+//1st: switch state reading timer - frequency at which switches are read
 Metro switch_timer = Metro(100);
-//2nd: CAN Transmission timer
+//2nd: CAN Transmission timer - frequency at which CAN packets are sent if switch states have not changed
 Metro CAN_TX = Metro(1000);
-//3rd: CAN Reception timer
+//3rd: CAN Reception timer - duration between CAN packets received (will trigger error if it expires)
 Metro CAN_RX = Metro(1000);
-//4th: Notification Timer
+//4th: Notification Timer - duration for which notification is displayed
 Metro notif_timer = Metro(2000);
-//5th: Display Timer
+//5th: Display Timer - frequency at which display will refresh if nothing changes
 Metro display_timer = Metro(500);
 //6th: Turn signal blinking timer
 Metro blinking_timer = Metro(500);
@@ -77,29 +85,32 @@ uint16_t CAN_errors;
 
 CAN_IO CanControl(CAN_CS,CAN_INT,CAN_BAUD_RATE,CAN_FREQ);
 
+//Declaring switch objects (for debouncing, based on the included Switch library)
 Switch cruisecontrol(ccp);
 Switch horn(hornp);
 
+//Declaring serLCD object (display, based on the NUserLCD library)
 serLCD screen(Serial1);
 
 //setup a display structure to store the shenanigans that we neeed to display on LCD
 struct LCD{
-  char CCdisplay;
-  char geardisplay;
-  String Lightsdisplay;
-  int SOCdisplay;
-  int Veldisplay;
-  boolean LTdisplay;
-  boolean RTdisplay;
-  boolean turnsignal_on;
-  char notification[16];
+  char CCdisplay;        //'C' = cruise control on, ' ' = cruise control off
+  char geardisplay;      //'F' = forward, 'R' = reverse, 'N' = neutral
+  String Lightsdisplay;  //"H" = headlights, "HZ" = hazardlights, " " = no lights
+  int SOCdisplay;        //state of charge (from CAN)
+  int Veldisplay;        //velocity (from CAN)
+  boolean LTdisplay;     //distinguishes left turn
+  boolean RTdisplay;     //distinguishes right turn
+  boolean turnsignal_on; //whether turn signal is on/off
+  //char notification[16]; //notification string
 };
 LCD steering_wheel;
 
-String situation = "nothing";
+//declares the string that will contain the information that will be displayed in the notification
+String situation;
 
 void setup() {
-  delay(100);
+  delay(100); //ask alexander
   // Pin Modes
   pinMode(fgp, INPUT_PULLUP);
   pinMode(rgp, INPUT_PULLUP);
@@ -119,7 +130,7 @@ void setup() {
    * PRO MICRO MUST BE PUT INTO PROGRAMMING MODE BEFORE
    * PROGRAMMING BY SETTING HAZARD/HEADLIGHT SWITCH TO
    * THE HAZARDS POSITION.
-   */  checkProgrammingMode();
+   */  checkProgrammingMode(); // ask alexander
 
   /*
    * CAN Setup
@@ -148,10 +159,12 @@ void setup() {
 #endif
 }
 
+//assigns appropriate value to the bit from the state of the pin
 inline void switchBitFromPin(byte pin, char& out, byte mask){
   switchBit(digitalRead(pin),out, mask);
 }
 
+//helper function for the above switchBitFromPin function
 inline void switchBit(bool b, char& out, byte mask) {
   if (b){
     BIT_SET(out,mask);
@@ -161,8 +174,7 @@ inline void switchBit(bool b, char& out, byte mask) {
   }
 }
 
-/*copy over the blink function from the LCD testing code, used to blink the sides of the display for the turning signals*/
-
+//blink function used for the turn signals
 inline void blnk(int a, boolean on){
   if (on)
   {
@@ -190,6 +202,7 @@ inline void blnk(int a, boolean on){
   }
 }
 
+//this is the non-notification display function (what is displayed onto LCD if it's not the notification)
 inline void defaultdisplay(){
   screen.clear();
   screen.setCursor(1,4);
@@ -215,6 +228,7 @@ inline void defaultdisplay(){
   }
 }
 
+//Notification display function
 inline void displayNotification(){
   screen.clear();
   screen.selectLine(1);
@@ -235,7 +249,8 @@ void loop() {
     switchBitFromPin(hzp,  young,HAZARDLIGHT);
     switchBitFromPin(ltp,  young,LEFT_TURN);
     switchBitFromPin(rtp,  young,RIGHT_TURN);
-
+    
+    //poll the cruisecontrol and horn and change value of bit accordingly
     cruisecontrol.poll();
     if(cruisecontrol.pushed()){
       BIT_FLIP(young,CRUISE_CONTROL);
@@ -248,20 +263,23 @@ void loop() {
   if (old != young || display_timer.check()){
 #ifdef DEBUG
     Serial.print("Display:");
-    Serial.println(display_timer.previous_millis);
+    Serial.println(display_timer.previous_millis); //ask alexander
 #endif
 
+    //possibility of switch statements?
     //Switch turnsignal_on on and off at regular intervals
     steering_wheel.turnsignal_on = !steering_wheel.turnsignal_on;
 
     //Display shenanigans
+    /*What is generally happening is the code is checking whether the switch states have changed by juxtaposing the young and old bytes and then
+    changing the members of the display structure accordingly and also changing the notification string*/
 
     if(!(~young & (FWD_GEAR|REV_GEAR)) && steering_wheel.geardisplay != 'N'){
       steering_wheel.geardisplay = 'N';
       situation = String("Neutral Gear");
       notif_timer.reset();
 #ifdef DEBUG
-      Serial.print("ResetNEU");
+      Serial.print("ResetNEU"); //ask
 #endif
     }
     if((~young & FWD_GEAR) && steering_wheel.geardisplay != 'F'){
@@ -338,7 +356,7 @@ void loop() {
 
   wdt_reset();
 
-  // Check whether a 
+  // Check whether a CAN packet is available to read
   if (CanControl.Available()){
     
     // Use available CAN packets (BMS SOC and MC Velocity) to assign values to appropriate members of the data structures
@@ -347,14 +365,14 @@ void loop() {
     {
       case BMS_SOC_ID:
       {
-        BMS_SOC packet(f);
+        BMS_SOC packet(f); //This is where we get the State of charge
         steering_wheel.SOCdisplay = packet.percent_SOC;
         CAN_RX.reset();
         break;
       }
       case MC_VELOCITY_ID:
       {
-        MC_Velocity packet(f);
+        MC_Velocity packet(f); // This is where we get the velocity
         steering_wheel.Veldisplay = packet.car_velocity;
         CAN_RX.reset();
         break;
