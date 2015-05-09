6952777372ad8f98a75bbd4522b9651ddafb82a7
cpraf104 <ethanpark2018@u.northwestern.edu>
2015-05-09 15:54:25 -0500

    Changes

--- SWControls/V2.0/SWControls/SWControls.ino
+++ SWControls/V2.0/SWControls/SWControls.ino
@@ -45,14 +45,14 @@ const int RIGHT = 15; //right turn signals
 const int LEFT = 1;   //left turn signals
 
 //set up pins that connect to switch terminals
-const int fgp =   9;  //forward gear
-const int rgp =   8;  //reverse gear
-const int hp =    7;  //headlights
-const int hzp =   6;  //hazardlights
-const int ccp =   3;  //cruise control
-const int hornp = 10; //horn
-const int ltp =   4;  //left turn
-const int rtp =   5;  //right turn
+const int fgp =   6; //9;  //forward gear
+const int rgp =   7; //8;  //reverse gear
+const int hp =    9; //7;  //headlights
+const int hzp =   8; //6;  //hazardlights
+const int ccp =   4; //3  //cruise control
+const int hornp = 5; //10; //horn
+const int ltp =   3; //4;  //left turn
+const int rtp =   20; //5;  //right turn
 
 //set up metro timer
 //1st: switch state reading timer - frequency at which switches are read
@@ -69,7 +69,7 @@ Metro display_timer = Metro(500);
 Metro blinking_timer = Metro(500);
 
 //CAN parameters
-const byte	   CAN_CS 	 = A0;
+const byte	   CAN_CS 	 = 10; //A0
 const byte	   CAN_INT	 = 1;
 const uint16_t CAN_BAUD_RATE = 1000;
 const byte     CAN_FREQ      = 16;
