680f1ee1f005ea52f3c1dc0b4a53e74d97701002
Nathan Lindquist <nallink@comcast.net>
2015-05-15 01:08:19 -0500

    Implemented no steering wheel connected mode for driver controls.

---
+++ DriverControls/v2.0/DriverControls/steering-defs.h
@@ -0,0 +1,24 @@
+/*
+ * steering-defs.h
+ * Contains constant definitions for resources that are normally
+ * controlled by the steering wheel, but may be connected
+ * directly to the driver controls.
+ */
+
+#ifndef STEERING_DEFS_H
+#define STEERING_DEFS_H
+
+// gearing
+const byte NEUTRAL_PIN = 45;
+const byte REVERSE_PIN = 47;
+
+// lights
+const byte LEFT_TURN_SW_PIN    = 0;
+const byte RIGHT_TURN_SW_PIN   = 0;
+const byte HEADLIGHT_SW_PIN    = 0;
+const byte HAZARDS_SW_PIN      = 0;
+
+// other
+const byte HORN_SW_PIN = 0;
+
+#endif
