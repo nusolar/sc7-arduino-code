24fca21274473b430d400d70d616ed49041f59f2
Ethan Park <cpraf104@gmail.com>
2017-06-03 12:40:58 -0500

    integrated temp sensor code
    integrated Charlie's temp sensor code into driver controls

--- DriverControls/v2.0/DriverControls/DriverControls.ino
+++ DriverControls/v2.0/DriverControls/DriverControls.ino
@@ -9,6 +9,7 @@
 #include <Metro.h>
 #include <SPI.h>
 #include "steering-defs.h"
+#include <OneWire.h>
 
 //------------------------------CONSTANTS----------------------------//
 // debugging
@@ -72,6 +73,7 @@ const int      MAX_CAN_PACKETS_PER_LOOP = 10; // Maximum number of receivable CA
 const bool     ENABLE_REGEN        = false;   // flag to enable/disable regen
 const uint16_t DC_ID               = 0x00C7;  // For SC7
 const uint16_t DC_SER_NO           = 0x0042;  // Don't panic!
+const float    MAX_TEMP            = 60.0;    // absolute max battery temp in celsius
 
 // steering wheel parameters
 const byte NEUTRAL_RAW = 0x03;
@@ -86,7 +88,6 @@ const float CONTINUOUS_CURRENT_THRESH   = 50000; // current may exceed this valu
 const int   CURRENT_BUFFER_SIZE         = 10;    // number of current values from BMS stored
 const int   OVERCURRENTS_ALLOWED        = 7;     // max number of overcurrent values allowed before trip
 
-
 // driver control errors
 const uint16_t MC_TIMEOUT  = 0x01; // motor controller timed out
 const uint16_t BMS_TIMEOUT = 0x02; // bms timed out
@@ -94,6 +95,9 @@ const uint16_t SW_TIMEOUT  = 0x04; // sw timed out
 const uint16_t SW_BAD_GEAR = 0x08; // bad gearing from steering wheel
 const uint16_t BMS_OVER_CURR = 0x10; // Detected BMS overcurrent, tripped car.
 const uint16_t RESET_MCP2515 = 0x20; // Had to reset the MCP2515
+
+//temp sensor
+OneWire ds(50);
 
 //----------------------------TYPE DEFINITIONS------------------------//
 /*
@@ -174,6 +178,10 @@ struct CarState {
   // errors
   uint16_t canErrorFlags; // keep track of errors with CAN bus
   uint16_t dcErrorFlags;  // keep track of other errors
+
+  //temperature
+  float celsius;
+  float fahrenheit;
 };
 
 //----------------------------DATA/VARIABLES---------------------------//
@@ -414,6 +422,12 @@ void updateState() {
       state.leftTurnOn = false;
     }
   }
+
+  //check temp sensors
+  tempsensor();
+  if(state.celsius >= MAX_TEMP){
+    state.tripped = true;
+  }
   
   // update cruise control state
   //if (!state.cruiseCtrlPrev && state.cruiseCtrl) { // cruise control just switched on
@@ -623,6 +637,106 @@ void checkErrors() {
        Serial.println(canControl.canstat_register);
     }
   }
+}
+
+void tempsensor() {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float func_celsius, func_fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    //Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  /*Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }*/
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  //Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  //Serial.print("  Data = ");
+  //Serial.print(present, HEX);
+  //Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  //Serial.print(" CRC=");
+  //Serial.print(OneWire::crc8(data, 8), HEX);
+  //Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  func_celsius = (float)raw / 16.0;
+  func_fahrenheit = func_celsius * 1.8 + 32.0;
+  state.celsius = func_celsius;
+  state.fahrenheit = func_fahrenheit;
+  Serial.print("  Temperature = ");
+  Serial.print(func_celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(func_fahrenheit);
+  Serial.println(" Fahrenheit");
 }
 
 //--------------------------MAIN FUNCTIONS---------------------------//
