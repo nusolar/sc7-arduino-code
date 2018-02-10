3e580d510f654e649f697704382f1b32e9386e9d
Christopher Lee <32558174+ChristopherHLee@users.noreply.github.com>
2018-02-10 13:13:11 -0600

    Add files via upload

--- SWControls/LCDtest/TheFinalScreen/TheFinalScreen.ino
+++ SWControls/LCDtest/TheFinalScreen/TheFinalScreen.ino
@@ -4,11 +4,13 @@
 
 #include <SPI.h>
 #include <RA8875.h>
+#include <Interface.h>
 
 #define RA8875_INT 4
 #define RA8875_CS 10
 
 #define RA8875_RESET 9
+
 
 #define DEBUG
 
@@ -35,7 +37,7 @@
 
 /*These are the bytes containing the above-mentioned bits. The two bytes will be used
   to compare the switch states and determine whether there has been a change or not.*/
-char young = 0xFF; //young is continuously assigned to the new switch states
+char young = 0xFF; //young is continuously assigned to the new switch states 255
 char old;          //old is the previous switch states
 
 //Conversion data for displayed measurements
@@ -54,15 +56,17 @@ char old;          //old is the previous switch states
 //set up pins that connect to switch terminals
 //const int fgp =   6;  //forward gear --> don't have these rn for sc7
 //const int rgp =   7;  //reverse gear
-const int headlightp =    A4;  //headlights
+const int hdp =    A4;  //headlights
 const int hzp =   A5;  //hazardlights
-const int brakep = A6; //brakes
-const int strobep =   A7;  //strobe light
-const int hornp = A8; //horn
-const int leftp =   A9;  //left turn
-const int rightp =   A10;  //right turn
+const int brkp = A6; //brakes
+const int strbp =   A7;  //strobe light
+const int hrnp = A8; //horn
+const int ltp =   A9;  //left turn
+const int rtp =   A10;  //right turn
 const int caninterruptp  = 14; // CAN interrupt
-const int canchipselp = 52; // CAN chip select
+const int canchipp = 52; // CAN chip select
+
+
 // miso, mosi, and sck as well
 
 //set up metro timer, these may be diff, check later
@@ -93,33 +97,50 @@ uint16_t errors;
 CAN_IO CanControl(CAN_CS, CAN_INT, CAN_BAUD_RATE, CAN_FREQ); //Try initializing without interrupts for now
 
 //Indicators:
-float VELOC;
-float BAT_CURRENT;
-float MIN_BAT;
-float ARRAY_CURRENT;
-float MAX_TEMPERATURE;
+float VELOC;              // Velocity (from CAN)
+float BAT_CURRENT;        // Battery Current (from CAN)
+float MIN_BAT;            
+float ARRAY_CURRENT;      // Array Current (from CAN)
+float MAX_TEMPERATURE;    // Max Pack Temperature (from CAN)
 
-String HAZARD_LIGHT;
-boolean LEFT_LIGHT;
-boolean RIGHT_LIGHT;
-boolean BRAKES;
-String Err;
+String HAZARD_LIGHT;      // Hazard Lights
+boolean LEFT_LIGHT;       // Left Turn Signal
+boolean RIGHT_LIGHT;      // Right Turn Signal
+boolean BRAKES;           // Indicate if brakes are deployed
+String Err;               // Error message
+boolean TRIPPED;
 
 //Declaring Functions
-void interface();
-void setup();
-void loop ();
+void interface();         // Implements LCD Display 
+void setup();             // Update  
+void loop ();             // 
+
 
 RA8875 tft = RA8875(RA8875_CS, RA8875_RESET);
 uint16_t tx, ty;
 
 void setup() {
   // put your setup code here, to run once:
+  
+  pinMode(hdp, INPUT_PULLUP) // headlights
+  pinMode(hzp, INPUT_PULLUP) // hazardlights
+  pinMode(brkp, INPUT_PULLUP) // brakes
+  pinMode(strbp, INPUT_PULLUP) // strobe lights
+  pinMode(hrn, INPUT_PULLUP) // horn 
+  pinMode(ltp, INPUT_PULLUP) // left turn
+  pinMode(rtp, INPUT_PULLUP) // right turn
+  pinMode(caninterruptp, INPUT_PULLUP) // CAN interrupt
+  pinMode(canchipp, INPUT_PULLUP) // CAN chip select
+
+  
+
   Serial.begin(9600);
   tft.begin(RA8875_800x480);
   tft.touchBegin(RA8875_INT);
   delay(3000);  //Allow LCD and MCP2515 to fully boot (Not sure what actual value to use with the new LCD).
   interface();
+
+  initializePins();
 
   CanControl.filters.setRB0(MASK_Sxxx, BMS_VOLT_CURR_ID, DC_TEMP_0_ID);
   CanControl.filters.setRB1(MASK_Sxxx, MTBA_FRAME0_REAR_LEFT_ID, DC_INFO_ID, 0); //**MC_VELOCITY_ID, **MC_PHASE_ID
@@ -127,78 +148,6 @@ void setup() {
 
 // Insert DEBUG and LOOPBACK steps
   
-}
-
-void interface() {
- tft.setBackgroundColor(RA8875_BLACK);
- tft.drawRect(5,5,390, 195, RA8875_WHITE);
- tft.drawRect(5,205,390, 270, RA8875_WHITE);
- tft.drawRect(400,5, 390, 470,RA8875_WHITE);
- 
- tft.changeMode(TEXT);//Speed Stuff
- tft.setTextColor(RA8875_WHITE, RA8875_BLACK);
- tft.setCursor (25,25);
- tft.setFontScale(1);
- tft.print("Speed: ");
- tft.setCursor(180,70);
- tft.setFontScale(5);
- tft.print(VELOC);
- tft.setCursor(335,160);
- tft.setFontScale(1);
- tft.print("mph");
-
- tft.setFontScale(1);//Array Current
- tft.setCursor(25,225);
- tft.print("Array(A): ");
- tft.setCursor (220,225);
- tft.print(ARRAY_CURRENT);
- 
- tft.setTextColor(RA8875_WHITE, RA8875_BLACK);//Minimum Battery Voltage
- tft.setCursor (25,285);
- tft.setFontScale(1);
- tft.print("Min V: ");
- tft.setCursor (220,285);
- tft.print(MIN_BAT);
- 
- tft.setTextColor(RA8875_WHITE, RA8875_BLACK);//Battery Current
- tft.setCursor (25,345);
- tft.setFontScale(1);
- tft.print("Batt A: "); 
- tft.setCursor (220,345);
- tft.print(BAT_CURRENT);
- 
- tft.setTextColor(RA8875_WHITE, RA8875_BLACK);//Max Temperature
- tft.setCursor (25,405);
- tft.setFontScale(1);
- tft.print("Max Temp: ");
- tft.setCursor (220,405);
- tft.print(MAX_TEMPERATURE);
-
- tft.drawRect(450,170,150,240,RA8875_WHITE);//Basic Car Model
- tft.drawEllipse(525,320,15,30,RA8875_WHITE);
- tft.drawRect(450,230,20,45,RA8875_WHITE);
- tft.drawRect(580,230,20,45,RA8875_WHITE);
- tft.drawRect(450,365,20,45,RA8875_WHITE);
- tft.drawRect(580,365,20,45,RA8875_WHITE);
- 
- tft.fillTriangle(455,150,495,150,475,190,RA8875_YELLOW);//Headlights
- tft.fillTriangle(595,150,555,150,575,190,RA8875_YELLOW);
- 
- tft.fillTriangle(440,440,470,425,470,455, RA8875_YELLOW);//Turn Signals
- tft.fillTriangle(610,440,580,425,580,455, RA8875_YELLOW);
- tft.fillRect(470,433,30,14,RA8875_YELLOW);
- tft.fillRect(550,433,30,14,RA8875_YELLOW);
-
- tft.setCursor(630,270);//Brakes
- tft.setFontScale(2);
- tft.setTextColor(RA8875_RED, RA8875_BLACK);
- tft.print("BRAKE");
-
- tft.setCursor(407,20);//Error
- tft.setFontScale(1);
- tft.setTextColor(RA8875_WHITE,RA8875_BLACK);
- tft.print("Error: ");
- tft.print(ERROR);
 }
 
 void loop()  {
@@ -215,35 +164,36 @@ void loop()  {
 #endif
     switch (f.id)
     {
-      case BMS_VOLT_CURR_ID:
+      case BMS_VOLT_CURR_ID: // Voltage/Current of battery 
+      
         {
           BMS_VoltageCurrent packet(f); //Get the voltage and current of the battery pack
-          steering_wheel.BCurrentdisplay = packet.current / 1000.0;
-          steering_wheel.BVoltagedisplay = packet.voltage / 1000.0;
+          BAT_CURRENT = packet.current / 1000.0;
+          BVoltagedisplay = packet.voltage / 1000.0;
           CAN_RX.reset();
           break;
         }
-      case MTBA_FRAME0_REAR_LEFT_ID: //19 inch diameter of wheels, figure out conversion factor
+      case MTBA_FRAME0_REAR_LEFT_ID: //Velocity: 19 inch diameter of wheels, figure out conversion factor
         {
           MTBA_F0_RLEFT packet(f);
           VELOC = motor_rotating_speed * RPM_TO_MPH;
           CAN_RX.reset();
           break;
         }
-      case DC_TEMP_0_ID:
+      case DC_TEMP_0_ID: // Get Max Pack Temp
         {
-          DC_Temp_0 packet(f); // Get Max Pack Temp
-          steering_wheel.MaxTempdisplay = packet.max_temp;
+          DC_Temp_0 packet(f); 
+          MAX_TEMPERATURE = packet.max_temp;
           CAN_RX.reset();
           break;
         }
       case DC_INFO_ID:
         {
           DC_Info packet(f); // Get Tripped state of vehicle
-          steering_wheel.tripped = packet.tripped;
-          if (steering_wheel.tripped) {
-            steering_wheel.notification = GENERIC_TRIP_STR;
-            notif_timer.reset();
+          TRIPPED = packet.tripped;
+          if (TRIPPED) {
+            ERROR = GENERIC_TRIP_STR;
+            notif_timer.resOPet();
           }
           CAN_RX.reset();
           break;
@@ -262,6 +212,7 @@ void loop()  {
           steering_wheel.telemetrydisplay = 'T';
           telmetry_timer.reset();
         }
+        
     }
   }
 }
