e370f8c509e305ba6706376b7f090734441448af
leo-c-ling99 <leoling99@gmail.com>
2019-04-18 20:23:30 -0500

    Changed LCD code to compile with new CAN Packets
    Need to finish mapping

--- SWControls/LCDtest/TheFinalScreen/TheFinalScreen.ino
+++ SWControls/LCDtest/TheFinalScreen/TheFinalScreen.ino
@@ -10,23 +10,22 @@
 
 // CAN Constants not set in the CAN library (SW branch)
 
-
 #define DEBUG
 
 /*Defining the bitwise functions (bitwise operators)
   We're using bits to store data because there are only 8 bytes available for use in a CAN packet.
   We can store all of the necessary data in a single byte and save space. This frees up space to
   send other data that we might need in the future between the steering wheel and the driver controls*/
-#define BIT(n)                   ( 1<<(n) )
-#define BIT_SET(y, mask)         ( y |=  (mask) )                       //Set the value of BIT = 1
-#define BIT_CLEAR(y, mask)       ( y &= ~(mask) )                       //Set the value of BIT = 0
-#define BIT_FLIP(y, mask)        ( y ^=  (mask) )                       //Flip the value of BIT
-#define BIT_DIFFERENT(y, x, mask)( y & mask == x & mask )               //See if two bits are different values
-#define BIT_CHECK(y, bit, mask)  ( BIT_DIFFERENT(y, (bit)*mask, mask) ) //check function utilizing the previous defined different function
+#define BIT(n) (1 << (n))
+#define BIT_SET(y, mask) (y |= (mask))                               //Set the value of BIT = 1
+#define BIT_CLEAR(y, mask) (y &= ~(mask))                            //Set the value of BIT = 0
+#define BIT_FLIP(y, mask) (y ^= (mask))                              //Flip the value of BIT
+#define BIT_DIFFERENT(y, x, mask) (y & mask == x & mask)             //See if two bits are different values
+#define BIT_CHECK(y, bit, mask) (BIT_DIFFERENT(y, (bit)*mask, mask)) //check function utilizing the previous defined different function
 
 //Defining the bit masks
-#define FWD_GEAR BIT(0)   // might be diff with the new motors
-#define REV_GEAR BIT(1)   // might be diff with the new motors
+#define FWD_GEAR BIT(0) // might be diff with the new motors
+#define REV_GEAR BIT(1) // might be diff with the new motors
 #define HEADLIGHT BIT(2)
 #define HAZARDLIGHT BIT(3)
 //#define LAP_TIMER BIT(4)
@@ -43,28 +42,27 @@ char old;          //old is the previous switch states
 #define RPM_TO_MPH 2.2369f //Change for correct values, diameter 19 inch
 
 //BMS Error Strings
-#define BMS_12VERR_STR              String("TRIP: 12V ERR")
-#define BMS_DRIVECONTROLSERR_STR    String("TRIP: DCTRLS ERR")
-#define BMS_CONTACTERR_STR          String("TRIP: CNTCR ERR")
-#define BMS_CURRENTERR_STR          String("TRIP: OVERCURRENT") // This currently doesn't do anything since the BMS doesn't report current trips
-#define BMS_OVVOLTAGE_STR           String("TRIP: OVER V")
-#define BMS_UNVOLTAGE_STR           String("TRIP: UNDER V")
-#define BMS_UNKERR_STR              String("TRIP: UNK. ERR")
-#define GENERIC_TRIP_STR            String("SOMETHING IS WRONG WITH YOUR CAR")
+#define BMS_12VERR_STR String("TRIP: 12V ERR")
+#define BMS_DRIVECONTROLSERR_STR String("TRIP: DCTRLS ERR")
+#define BMS_CONTACTERR_STR String("TRIP: CNTCR ERR")
+#define BMS_CURRENTERR_STR String("TRIP: OVERCURRENT") // This currently doesn't do anything since the BMS doesn't report current trips
+#define BMS_OVVOLTAGE_STR String("TRIP: OVER V")
+#define BMS_UNVOLTAGE_STR String("TRIP: UNDER V")
+#define BMS_UNKERR_STR String("TRIP: UNK. ERR")
+#define GENERIC_TRIP_STR String("SOMETHING IS WRONG WITH YOUR CAR")
 
 //set up pins that connect to switch terminals
 //const int fgp =   6  //forward gear --> don't have these rn for sc7
 //const int rgp =   7;  //reverse gear
-#define hdp A4  //headlights
-#define hzp A5  //hazardlights
-#define brkp A6 //brakes
-#define strbp A7  //strobe light
-#define hrnp A8 //horn
-#define ltp A9  //left turn
-#define rtp A10  //right turn
+#define hdp A4           //headlights
+#define hzp A5           //hazardlights
+#define brkp A6          //brakes
+#define strbp A7         //strobe light
+#define hrnp A8          //horn
+#define ltp A9           //left turn
+#define rtp A10          //right turn
 #define caninterruptp 14 // CAN interrupt
-#define canchipp 52 // CAN chip select
-
+#define canchipp 52      // CAN chip select
 
 // miso, mosi, and sck as well
 
@@ -87,47 +85,45 @@ Metro debug_timer = Metro(200);
 Metro telmetry_timer = Metro(2500);
 
 //CAN parameters --> check these, may be diff
-const byte     CAN_CS    = 10;
-const byte     CAN_INT   = 2; // Interrupt #1
+const byte CAN_CS = 10;
+const byte CAN_INT = 2; // Interrupt #1
 const uint16_t CAN_BAUD_RATE = 500;
-const byte     CAN_FREQ      = 16;
+const byte CAN_FREQ = 16;
 uint16_t errors;
 
 CAN_IO CanControl(CAN_CS, CAN_INT, CAN_BAUD_RATE, CAN_FREQ); //Try initializing without interrupts for now
 
 //Indicators:
-float VELOC;              // Velocity (from CAN)
-float BAT_CURRENT;        // Battery Current (from CAN)
-float MIN_BAT;            
-float ARRAY_CURRENT;      // Array Current (from CAN)
-float MAX_TEMPERATURE;    // Max Pack Temperature (from CAN)
+float VELOC;       // Velocity (from CAN)
+float BAT_CURRENT; // Battery Current (from CAN)
+float MIN_BAT;
+float ARRAY_CURRENT;   // Array Current (from CAN)
+float MAX_TEMPERATURE; // Max Pack Temperature (from CAN)
 
-String HAZARD_LIGHT;      // Hazard Lights
-boolean LEFT_LIGHT;       // Left Turn Signal
-boolean RIGHT_LIGHT;      // Right Turn Signal
-boolean BRAKES;           // Indicate if brakes are deployed
-String Err;               // Error message
+String HAZARD_LIGHT; // Hazard Lights
+boolean LEFT_LIGHT;  // Left Turn Signal
+boolean RIGHT_LIGHT; // Right Turn Signal
+boolean BRAKES;      // Indicate if brakes are deployed
+String Err;          // Error message
 boolean TRIPPED;
-
 
 uint16_t tx, ty;
 
 sc7SW_UI swLCD = sc7SW_UI(RA8875_INT, RA8875_CS, RA8875_RESET); // Initializes screen
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  
-  pinMode(hdp, INPUT_PULLUP); // headlights
-  pinMode(hzp, INPUT_PULLUP); // hazardlights
-  pinMode(brkp, INPUT_PULLUP); // brakes
-  pinMode(strbp, INPUT_PULLUP); // strobe lights
-  pinMode(hrnp, INPUT_PULLUP); // horn 
-  pinMode(ltp, INPUT_PULLUP); // left turn
-  pinMode(rtp, INPUT_PULLUP); // right turn
-  pinMode(caninterruptp, INPUT_PULLUP); // CAN interrupt
-  pinMode(canchipp, INPUT_PULLUP); // CAN chip select
 
-  
+  pinMode(hdp, INPUT_PULLUP);           // headlights
+  pinMode(hzp, INPUT_PULLUP);           // hazardlights
+  pinMode(brkp, INPUT_PULLUP);          // brakes
+  pinMode(strbp, INPUT_PULLUP);         // strobe lights
+  pinMode(hrnp, INPUT_PULLUP);          // horn
+  pinMode(ltp, INPUT_PULLUP);           // left turn
+  pinMode(rtp, INPUT_PULLUP);           // right turn
+  pinMode(caninterruptp, INPUT_PULLUP); // CAN interrupt
+  pinMode(canchipp, INPUT_PULLUP);      // CAN chip select
 
   Serial.begin(9600);
   delay(3000);
@@ -137,100 +133,109 @@ void setup() {
   delay(3000);  //Allow LCD and MCP2515 to fully boot (Not sure what actual value to use with the new LCD).
   //interface(); */
 
-  //initializePins(); // Part of SWControls 
+  //initializePins(); // Part of SWControls
+  const uint16_t RXM0 = MASK_Sxxx;
+  const uint16_t RXF0 = 0;              // Match any steering_wheel packet (because mask is Sx00)
+  const uint16_t RXF1 = BMS19_VCSOC_ID; // Can't put 0 here, otherwise it will match all packets that start with 0.
 
-  CanControl.filters.setRB0(MASK_Sxxx, BMS_VOLT_CURR_ID, DC_TEMP_0_ID);
-  CanControl.filters.setRB1(MASK_Sxxx, MTBA_FRAME0_REAR_LEFT_ID, DC_INFO_ID, 0, 0); //**MC_VELOCITY_ID, **MC_PHASE_ID
-  CanControl.Setup(RX0IE | RX1IE);
+  const uint16_t RXM1 = MASK_Sxxx;
+  const uint16_t RXF2 = SW_DATA_ID;
+  const uint16_t RXF3 = 0;                                      // No longer relevant, but keeping here to have a value
+  const uint16_t RXF4 = (MTBA_FRAME0_REAR_LEFT_ID & MASK_Sxxx); // Not sure if necessary, but MTBA IDs are 29 bits
+  const uint16_t RXF5 = (MTBA_FRAME0_REAR_RIGHT_ID & MASK_Sxxx);
+  CanControl.filters.setRB0(MASK_Sxxx, RXF0, RXF1);
+  CanControl.filters.setRB1(MASK_Sxxx, RXF2, RXF3, RXF4, RXF5); //**MC_VELOCITY_ID, **MC_PHASE_ID
+  CanControl.Setup(RX0IE | RX1IE | TX1IE | TX2IE | TX0IE);
 
-// Insert DEBUG and LOOPBACK steps
-  
+  // Insert DEBUG and LOOPBACK steps
 }
 
-void loop()  {
-    // Fetch any potential messages from the MCP2515
-    CanControl.Fetch();
-    
-    if (CanControl.Available()) {
+void loop()
+{
+  // Fetch any potential messages from the MCP2515
+  CanControl.Fetch();
+
+  if (CanControl.Available())
+  {
 
     // Use available CAN packets to assign values to appropriate members of the data structures
-    Frame& f = CanControl.Read();
+    Frame &f = CanControl.Read();
 #ifdef DEBUG
-    Serial.print("Received: " );
+    Serial.print("Received: ");
     Serial.println(f.id, HEX);
 #endif
     switch (f.id)
     {
-      case BMS19_VCSOC_ID: // Voltage/Current of battery 
-        {
-          BMS19_VCSOC packet(f); //Get the voltage and current of the battery pack
-          BAT_CURRENT = packet.current / 1000.0;
-          swLCD.updateBatCurr(BAT_CURRENT);
-          CAN_RX.reset();
-          break;
-        }
-      case BMS19_MinMaxTemp_ID:
-        {
-          BMS19_MinMaxTemp packet(f);
-          MAX_TEMPERATURE = packet.maxTemp;
-          swLCD.updateMaxTemp(MAX_TEMPERATURE);
-          CAN_RX.reset();
-          break;
-        }
-      case MTBA_FRAME0_REAR_LEFT_ID: //Velocity: 19 inch diameter of wheels, figure out conversion factor
-        {
-          MTBA_F0_RLeft packet(f);
-          VELOC = packet.motor_rotating_speed * RPM_TO_MPH;
-          CAN_RX.reset();
-          break;
-        } 
-      /* case BMS19_BATT_STAT_ID: 
+    case BMS19_VCSOC_ID: // Voltage/Current of battery
+    {
+      BMS19_VCSOC packet(f); //Get the voltage and current of the battery pack
+      BAT_CURRENT = packet.current / 1000.0;
+      swLCD.updateBatCurr(BAT_CURRENT);
+      CAN_RX.reset();
+      break;
+    }
+    case BMS19_MinMaxTemp_ID:
+    {
+      BMS19_MinMaxTemp packet(f);
+      MAX_TEMPERATURE = packet.maxTemp;
+      swLCD.updateMaxTemp(MAX_TEMPERATURE);
+      CAN_RX.reset();
+      break;
+    }
+    case MTBA_FRAME0_REAR_LEFT_ID: //Velocity: 19 inch diameter of wheels, figure out conversion factor
+    {
+      MTBA_F0_RLeft packet(f);
+      VELOC = packet.motor_rotating_speed * RPM_TO_MPH;
+      CAN_RX.reset();
+      break;
+    }
+    /* case BMS19_BATT_STAT_ID: 
         {
           BMS19_Batt_Stat packet(f);
           VELOC = packet.motor_rotating_speed * RPM_TO_MPH;
           CAN_RX.reset();
           break;
         } */
-      case DC_TEMP_0_ID: // Get Max Pack Temp
-        {
-          DC_Temp_0 packet(f); 
-          MAX_TEMPERATURE = packet.max_temp;
-          swLCD.updateMaxTemp(MAX_TEMPERATURE);
-          CAN_RX.reset();
-          break;
-        }
-      case DC_INFO_ID:
-        {
-          DC_Info packet(f); // Get Tripped state of vehicle
-          TRIPPED = packet.tripped;
-          if (TRIPPED) {
-            String errorStr = GENERIC_TRIP_STR;
-            swLCD.updateError(errorStr);
-            //notif_timer.resOPet(); //res0pet doesn't exsist
-          }
-          else
-          {
-            swLCD.updateError("");
-          }
-          
-          CAN_RX.reset();
-          break;
-        }
-      case BMS_STATUS_EXT_ID:
-        {
-        BMS_Status_Ext packet(f); // extract the flags
-        //if      (packet.flags & BMS_Status_Ext::F_OVERVOLTAGE)    {notif_timer.reset(); steering_wheel.notification = BMS_OVVOLTAGE_STR;}
-        //else if (packet.flags & BMS_Status_Ext::F_UNDERVOLTAGE)   {notif_timer.reset(); steering_wheel.notification = BMS_UNVOLTAGE_STR;}
-        //else if (packet.flags & BMS_Status_Ext::F_12VLOW)         {notif_timer.reset(); steering_wheel.notification = BMS_12VERR_STR;}
-        CAN_RX.reset();
-        break;
-        }
-      case TEL_HEARTBEAT_ID:
-        {
-          //steering_wheel.telemetrydisplay = 'T';
-          //telmetry_timer.reset();
-        }
-        
+    case DC_TEMP_0_ID: // Get Max Pack Temp
+    {
+      DC_Temp_0 packet(f);
+      MAX_TEMPERATURE = packet.max_temp;
+      swLCD.updateMaxTemp(MAX_TEMPERATURE);
+      CAN_RX.reset();
+      break;
+    }
+    case DC_INFO_ID:
+    {
+      DC_Info packet(f); // Get Tripped state of vehicle
+      TRIPPED = packet.tripped;
+      if (TRIPPED)
+      {
+        String errorStr = GENERIC_TRIP_STR;
+        swLCD.updateError(errorStr);
+        //notif_timer.resOPet(); //res0pet doesn't exsist
+      }
+      else
+      {
+        swLCD.updateError("");
+      }
+
+      CAN_RX.reset();
+      break;
+    }
+    /*case BMS_STATUS_EXT_ID:
+    {
+      BMS_Status_Ext packet(f); // extract the flags
+      //if      (packet.flags & BMS_Status_Ext::F_OVERVOLTAGE)    {notif_timer.reset(); steering_wheel.notification = BMS_OVVOLTAGE_STR;}
+      //else if (packet.flags & BMS_Status_Ext::F_UNDERVOLTAGE)   {notif_timer.reset(); steering_wheel.notification = BMS_UNVOLTAGE_STR;}
+      //else if (packet.flags & BMS_Status_Ext::F_12VLOW)         {notif_timer.reset(); steering_wheel.notification = BMS_12VERR_STR;}
+      CAN_RX.reset();
+      break;
+    }
+    case TEL_HEARTBEAT_ID:
+    {
+      //steering_wheel.telemetrydisplay = 'T';
+      //telmetry_timer.reset();
+    } */
     }
   }
 }
